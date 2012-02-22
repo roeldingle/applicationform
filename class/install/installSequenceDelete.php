83adb0e5c1d56c749863b36768b428e2f5fdf1e0
sdk_roel <sdk_roel@simplexi.com>
2012-02-22 15:12:51 +0800

    adminPage finish/seq added

--- class/install/installSequenceDelete.php
+++ class/install/installSequenceDelete.php
@@ -3,12 +3,14 @@ class installSequenceDelete
 {
     function run($aArgs)
     {
-    $dDeleted = common()->modelExec()->deleteContentsBySeq($aArgs['seq']);
+    	
+   	 $dDeleted = common()->modelExec()->deleteContentsBySeq($aArgs['seq']);
     	
     	if ($dDeleted !== false) {
     		return true;
     	} else {
     		return false;
     	} 
+    	
     }
 }
